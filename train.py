577cd7d3354d6c8bcfa7edb4546057ef0d80b041
Zahra Hosseini <zahra.hosseini@sickkids.ca>
2025-04-03 16:16:50 -0400

    add train script

---
+++ train.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+X, y = load_iris(return_X_y=True)
+model = RandomForestClassifier()
+model.fit(X, y)
+joblib.dump(model, "model.joblib")
+
+print("Model saved as model.joblib")
